38ebc781c0667e366094a9d861d5281fad9f735e
Benjamin Saunders <besaunde@sfu.ca>
2009-11-12 04:34:15 -0800

    Stubbed entry point and main loop.

--- fwoosh.asd
+++ fwoosh.asd
@@ -2,4 +2,5 @@
   :maintainer "Benjamin Saunders"
   :depends-on (#:cl-opengl #:cl-glfw)
   :components
-  ((:file "package")))
+  ((:file "package")
+   (:file "fwoosh")))
---
+++ fwoosh.lisp
@@ -0,0 +1,14 @@
+(in-package :fwoosh)
+
+(defun fwoosh ()
+  (glfw:init)
+  (unwind-protect (progn
+                    (glfw:open-window 0 0 0 0 0 0 16 0)
+                    (main-loop))
+    (glfw:terminate)))
+
+(defun main-loop ()
+  (loop while (= 1 (glfw:get-window-param glfw:+opened+)) do
+       (gl:clear :color-buffer :depth-buffer)
+       (glfw:swap-buffers)
+       (sleep (/ 60))))
