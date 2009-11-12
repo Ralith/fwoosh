258a42f91028ad60676d39235975ee36c30b52d9
Benjamin Saunders <besaunde@sfu.ca>
2009-11-12 15:31:48 -0800

    Added trivial render code.

--- fwoosh.lisp
+++ fwoosh.lisp
@@ -4,11 +4,23 @@
   (glfw:init)
   (unwind-protect (progn
                     (glfw:open-window 0 0 0 0 0 0 16 0)
+                    (init-resources)
                     (main-loop))
     (glfw:terminate)))
+
+(defun init-resources ()
+  (format t "OpenGL version ~a~%" (gl:get-string :version))
+  (gl:clear-color 0 0 0 0)
+  (gl:ortho 0 1 0 1 -1 1))
 
 (defun main-loop ()
   (loop while (/= 0 (glfw:get-window-param glfw:+opened+)) do
        (gl:clear :color-buffer :depth-buffer)
+       (gl:with-primitive :polygon
+         (gl:color 0 1 0)
+         (gl:vertex 1/4 1/4 0)
+         (gl:vertex 3/4 1/4 0)
+         (gl:vertex 3/4 3/4 0)
+         (gl:vertex 1/4 3/4 0))
        (glfw:swap-buffers)
        (sleep (/ 60))))
